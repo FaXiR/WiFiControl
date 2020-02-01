516770887571b536f64d8d60ce7f107a2f7957cd
FaXiR <fFaXiR@yandex.ru>
2020-02-01 14:22:44 +0500

    Написание класса CMDbase (Взаимодействие с CMD)

--- WiFiControlLogic/Modules/CMDbase.cs
+++ WiFiControlLogic/Modules/CMDbase.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.IO;
 
 namespace WiFiControlLogic.Modules
 {
@@ -11,5 +9,41 @@ namespace WiFiControlLogic.Modules
     /// </summary>
     class CMDbase
     {
+        /// <summary>
+        /// Скрытное исполнение CMD команды
+        /// </summary>
+        /// <param name="CMDcommand">Команда для CMD. Наример (@"netstat /e | find ""Байт""")</param>
+        /// <returns>Ответ консоли</returns>
+        protected string CMDexecute(string CMDcommand)
+        {
+            ProcessStartInfo psiOpt = new ProcessStartInfo(@"cmd.exe", "/C" + CMDcommand)
+            {
+                //Задаем параметры запуска как скрытые
+                WindowStyle = ProcessWindowStyle.Hidden,
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+
+            Process procCommand = Process.Start(psiOpt);
+            StreamReader srIncoming = procCommand.StandardOutput;
+            procCommand.WaitForExit();
+
+            string result = srIncoming.ReadToEnd();
+            if (result == "")
+            {
+                throw new NullReferenceException("Пустой ответ, возможно, ошибка в команде: " + CMDcommand);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// Исполнение CMD команды в отображаемом окне без возврата данных
+        /// </summary>
+        /// <param name="CMDcommand">Команда для CMD. Наример (@"netstat /e | find ""Байт""" && Timeout /t 3)</param>
+        protected void CMDexecuteShow(string CMDcommand)
+        {
+            Process.Start(@"cmd.exe", "/C" + CMDcommand);
+        }
     }
 }

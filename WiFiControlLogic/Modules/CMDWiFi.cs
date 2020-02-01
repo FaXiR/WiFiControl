d7a7d757288e80430b8bb0c002f5d64a3898882e
FaXiR <fFaXiR@yandex.ru>
2020-02-01 14:37:58 +0500

    Неполное написание класса CMDWiF

--- WiFiControlLogic/Modules/CMDWiFi.cs
+++ WiFiControlLogic/Modules/CMDWiFi.cs
@@ -1,15 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace WiFiControlLogic.Modules
 {
     /// <summary>
-    /// Получает ифнормацю о состоянии WiFi и подключениях. А также управление состоянием WiFi
+    /// Получает ифнормацю о состоянии WiFi и подключениях. А также управляет состоянием WiFi
     /// </summary>
-    class CMDWiFi
+    class CMDWiFi : CMDbase
     {
+        public string WiFiStatus { private set; get; }
+        public string ReceivedWiFiName { private set; get; }
+        public List<string> ListMAC { private set; get; }
+
+        /// <summary>
+        /// Запускает видимое CMD окно с командой netsh wlan show hostednetwork && Timeout /t 
+        /// </summary>
+        public void CMDShowHostedNetwork()
+        {
+            CMDexecuteShow("netsh wlan show hostednetwork && Timeout /t 3");
+        }
+
+        /// <summary>
+        /// Запускает WiFi
+        /// </summary>
+        public void EnabledWiFi()
+        {
+            CMDexecute("netsh wlan start hostednetwork");
+        }
+
+        /// <summary>
+        /// Перезапускает WiFi
+        /// </summary>
+        public void RestartWiFi()
+        {
+            CMDexecute("netsh wlan stop hostednetwork && netsh wlan start hostednetwork");
+        }
+
+        /// <summary>
+        /// Выключает WiFi
+        /// </summary>
+        public void DisabledWiFi()
+        {
+            CMDexecute("netsh wlan stop hostednetwork");
+        }
+
+        /// <summary>
+        /// Обновление данных в WiFiStatus, ReceivedWiFiName и ListMAC.
+        /// </summary>
+        public void UpdateInfo()
+        {
+            string result = CMDexecute("netsh wlan show hostednetwork");
+
+            if (result.IndexOf("Параметры размещенной сети") == -1)
+            {
+                throw new NullReferenceException(result);
+            }
+
+            WiFiStatus = null;
+            ReceivedWiFiName = null;
+            ListMAC.Clear();
+
+            // TODO: допилить обновление данных
+        }
     }
 }

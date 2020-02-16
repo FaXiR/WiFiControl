98ccc4b58ebe74bbda8021841dfe69baa1126958
FaXiR <fFaXiR@yandex.ru>
2020-02-16 16:16:33 +0500

    Добавление класса CMDCommand

---
+++ WiFiControlLogic/Modules/CMDCommand.cs
@@ -0,0 +1,22 @@
+﻿namespace WiFiControlLogic.Modules
+{
+    public class CMDCommand : CMDbase
+    {
+        /// <summary>
+        /// Выполнение CMD комманды 
+        /// </summary>
+        /// <param name="CMDcommand">Команда для выполнения</param>
+        public string ExecuteCommand(string CMDcommand)
+        {
+            return CMDexecute(CMDcommand);
+        }
+
+        /// <summary>
+        /// Принудительное выключение ПК с игнорированием о сохранении данных и без задержки
+        /// </summary>
+        public void ExecuteForceShutdown()
+        {
+            CMDexecute("shutdown -f -s -t 0");
+        }
+    }
+}
